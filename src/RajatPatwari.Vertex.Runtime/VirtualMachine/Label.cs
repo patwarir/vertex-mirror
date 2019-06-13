d2899a0394bd24545a4bd6be7d398fc456777b65
Rajat Patwari <rajatpatwari@gmail.com>
2019-06-13 14:16:11 -0400

    Start on a redesign of the language.

---
+++ src/RajatPatwari.Vertex.Runtime/VirtualMachine/Label.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RajatPatwari.Vertex.Runtime.VirtualMachine
+{
+    public readonly struct Label : IComparable<Label>
+    {
+        public string Name { get; }
+
+        public ushort Position { get; }
+
+        public Label(string name, ushort position)
+        {
+            Name = name;
+            Position = position;
+        }
+
+        public int CompareTo(Label other) =>
+            Position.CompareTo(other.Position);
+    }
+}
---
+++ src/RajatPatwari.Vertex.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using RajatPatwari.Vertex.Runtime;
+using RajatPatwari.Vertex.Runtime.VirtualMachine;
+using System;
+using System.IO;
+
+namespace RajatPatwari.Vertex.Test
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            /*Console.Write("Please enter a file path: ");
+            var path = Console.ReadLine();
+            Console.WriteLine();
+
+            var code = File.ReadAllText(path);*/
+
+            var main = new Function("main", Datatype.Void);
+
+            main.Locals.Add(0, Datatype.Integer, 4L);
+
+            main.Buffer.WriteOperationCode(OperationCode.LoadLocal);
+            main.Buffer.WriteByte(0);
+
+            main.Buffer.WriteOperationCode(OperationCode.Negate);
+
+            main.Buffer.WriteOperationCode(OperationCode.Call);
+            main.Buffer.WriteDatatype(Datatype.Void);
+            main.Buffer.WriteString("$System.Console.WriteLine");
+            main.Buffer.WriteDatatypes(new[] { Datatype.Integer });
+
+            var interpreter = new Interpreter(new[] { main });
+            interpreter.Run();
+        }
+    }
+}
