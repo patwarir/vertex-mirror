f81d4b03a18ca63cdb2ac3658d11c9382d440abd
Rajat Patwari <rajatpatwari@gmail.com>
2019-06-14 16:41:10 -0400

    Finish the core of the language.

--- src/RajatPatwari.Vertex.Test/Program.cs
+++ src/RajatPatwari.Vertex.Test/Program.cs
@@ -1,5 +1,4 @@
 ﻿using RajatPatwari.Vertex.Runtime;
-using RajatPatwari.Vertex.Runtime.VirtualMachine;
 using System;
 using System.IO;
 
@@ -9,38 +8,16 @@ namespace RajatPatwari.Vertex.Test
     {
         public static void Main()
         {
-            Console.Write("Please enter a file path: ");
+            /*Console.Write("Please enter a file path: ");
             var path = Console.ReadLine();
-            Console.WriteLine();
+            Console.WriteLine();*/
 
-            var code = File.ReadAllText(path);
+            var code = File.ReadAllText(/*path*/@"C:\Users\patwa\Desktop\Projects\vertex\samples\Full Test\main.vir");
 
-            // Interpreter Test
+            var parser = new Parser(code);
+            parser.Run();
 
-            var main = new Function("main", Datatype.Void);
-
-            main.Locals.Add(0, Datatype.Integer, 4L);
-            main.Locals.Add(1, Datatype.Integer, 2L);
-
-            main.Buffer.WriteOperationCode(OperationCode.LoadLocal);
-            main.Buffer.WriteByte(0);
-
-            main.Buffer.WriteOperationCode(OperationCode.LoadLocal);
-            main.Buffer.WriteByte(1);
-
-            main.Buffer.WriteOperationCode(OperationCode.Subtract);
-
-            main.Buffer.WriteOperationCode(OperationCode.LoadInteger);
-            main.Buffer.WriteInteger(1L);
-
-            main.Buffer.WriteOperationCode(OperationCode.CheckGreater);
-
-            main.Buffer.WriteOperationCode(OperationCode.Call);
-            main.Buffer.WriteDatatype(Datatype.Void);
-            main.Buffer.WriteString("$System.Console.WriteLine");
-            main.Buffer.WriteDatatypes(new[] { Datatype.Boolean });
-
-            var interpreter = new Interpreter(new[] { main });
+            var interpreter = new Interpreter(parser.Functions);
             interpreter.Run();
         }
     }
