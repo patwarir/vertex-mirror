a03e48053e021f5cff14c8a83ccdffb3b460dd83
Rajat Patwari <rajatpatwari@gmail.com>
2019-11-01 21:19:58 -0400

    Try to rewrite the Virtual Machine again to only use stack-based values.

--- RajatPatwari.Vertex.Runtime/Program.cs
+++ RajatPatwari.Vertex.Runtime/Program.cs
@@ -1,5 +1,4 @@
 ﻿using RajatPatwari.Vertex.Runtime.VirtualMachine;
-using System.Collections.Generic;
 
 namespace RajatPatwari.Vertex.Runtime
 {
@@ -7,49 +6,7 @@ namespace RajatPatwari.Vertex.Runtime
     {
         public static void Main()
         {
-            var function = new FunctionImplementation("rajatpatwari_test::main", Datatype.Void);
-            function.ParameterValues.Add((Datatype.String, "Hello, World!"));
-            function.Locals.Add((Datatype.String, string.Empty));
-
-            function.Buffer.WriteOperationCode(OperationCode.LoadParameter);
-            function.Buffer.WriteIndex(0);
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sio::ln_str", Datatype.String, new Datatype[0]));
-
-            function.Buffer.WriteOperationCode(OperationCode.LoadLiteral);
-            function.Buffer.WriteDatatype(Datatype.String);
-            function.Buffer.WriteString("It Works!");
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sfn::concat", Datatype.String, new Datatype[] { Datatype.String, Datatype.String }));
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sfn::concat", Datatype.String, new Datatype[] { Datatype.String, Datatype.String }));
-
-            function.Buffer.WriteOperationCode(OperationCode.SetLocal);
-            function.Buffer.WriteIndex(0);
-
-            function.Buffer.WriteOperationCode(OperationCode.LoadLocal);
-            function.Buffer.WriteIndex(0);
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sio::writeln", Datatype.Void, new Datatype[] { Datatype.String }));
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sio::read", Datatype.Void, new Datatype[0]));
-
-            function.Buffer.WriteOperationCode(OperationCode.Call);
-            function.Buffer.WriteDeclaration(new FunctionDeclaration(
-                "std.sio::clear", Datatype.Void, new Datatype[0]));
-
-            var interpreter = new Interpreter(new List<FunctionImplementation>() { function });
-            interpreter.Run();
+            var @var = ValueFactory.CreateScalarValue(0x32UL);
         }
     }
 }
--- RajatPatwari.Vertex.Runtime/VirtualMachine.cs
+++ RajatPatwari.Vertex.Runtime/VirtualMachine.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RajatPatwari.Vertex.Runtime.VirtualMachine
 {
@@ -10,9 +6,72 @@ namespace RajatPatwari.Vertex.Runtime.VirtualMachine
     {
         Void,
         Boolean,
-        Integer,
-        Float,
+        TinySigned,
+        TinyUnsigned,
+        ShortSigned,
+        ShortUnsigned,
+        MediumSigned,
+        MediumUnsigned,
+        LongSigned,
+        LongUnsigned,
+        FloatSingle,
+        FloatDouble,
+        Character,
         String
+    }
+
+    public interface IValue
+    {
+        public Datatype Datatype { get; }
+    }
+
+    public readonly struct ScalarValue<T> : IValue
+        where T : struct
+    {
+        public Datatype Datatype { get; }
+
+        public T Value { get; }
+
+        internal ScalarValue(in T value)
+        {
+            Value = value;
+            Datatype = value switch
+            {
+                bool _ => Datatype.Boolean,
+                sbyte _ => Datatype.TinySigned,
+                byte _ => Datatype.TinyUnsigned,
+                short _ => Datatype.ShortSigned,
+                ushort _ => Datatype.ShortUnsigned,
+                int _ => Datatype.MediumSigned,
+                uint _ => Datatype.MediumUnsigned,
+                long _ => Datatype.LongSigned,
+                ulong _ => Datatype.LongUnsigned,
+                float _ => Datatype.FloatSingle,
+                double _ => Datatype.FloatDouble,
+                char _ => Datatype.Character,
+                _ => throw new ArgumentException(nameof(value))
+            };
+        }
+    }
+
+    public sealed class StringValue : IValue
+    {
+        public Datatype Datatype { get; } = Datatype.String;
+
+        public string Value { get; } = string.Empty;
+
+        internal StringValue(string value) =>
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public static class ValueFactory
+    {
+        public static ScalarValue<T> CreateScalarValue<T>(in T value)
+            where T : struct =>
+            new ScalarValue<T>(value);
+
+        public static StringValue CreateStringValue(string value) =>
+            new StringValue(value) ?? throw new ArgumentNullException(nameof(value));
     }
 
     public enum OperationCode : byte
@@ -21,6 +80,7 @@ namespace RajatPatwari.Vertex.Runtime.VirtualMachine
 
         JumpAlways,
         JumpTrue,
+        JumpFalse,
 
         Call,
         Return,
@@ -39,200 +99,16 @@ namespace RajatPatwari.Vertex.Runtime.VirtualMachine
         SetLocal
     }
 
-    public sealed class Buffer : IEnumerable<byte>
+    public readonly struct Label
     {
-        private readonly IList<byte> _stream = new List<byte>();
+        public string Name { get; }
 
-        public int Length =>
-            _stream.Count;
+        public int Position { get; }
 
-        private byte Read(int position) =>
-            _stream[position];
-
-        public Datatype ReadDatatype(int position) =>
-            (Datatype)Read(position);
-
-        public IEnumerable<Datatype> ReadDatatypes(int position)
+        public Label(string name, int position)
         {
-            var length = Read(position++);
-
-            var datatypes = new List<Datatype>();
-            for (var index = position; index < position + length; index++)
-                datatypes.Add(ReadDatatype(index));
-            return datatypes;
+            Name = name;
+            Position = position;
         }
-
-        public OperationCode ReadOperationCode(int position) =>
-            (OperationCode)Read(position);
-
-        public byte ReadIndex(int position) =>
-            Read(position);
-
-        public string ReadIdentifier(int position)
-        {
-            var length = Read(position++);
-
-            var builder = new StringBuilder();
-            for (var index = position; index < position + length; index++)
-                builder.Append((char)Read(index));
-            return builder.ToString();
-        }
-
-        public FunctionDeclaration ReadDeclaration(int position)
-        {
-            var qualifiedName = ReadIdentifier(position);
-            position += 1 + qualifiedName.Length;
-
-            var parameters = ReadDatatypes(position);
-            position += 1 + parameters.Count();
-
-            var @return = ReadDatatype(position++);
-            return new FunctionDeclaration(qualifiedName, @return, parameters);
-        }
-
-        #region Literal Reads
-
-        public bool ReadBoolean(int position) =>
-            BitConverter.ToBoolean(_stream.ToArray(), position);
-
-        public long ReadInteger(int position) =>
-            BitConverter.ToInt64(_stream.ToArray(), position);
-
-        public double ReadFloat(int position) =>
-            BitConverter.ToDouble(_stream.ToArray(), position);
-
-        public string ReadString(int position)
-        {
-            var length = Read(position++);
-
-            var builder = new StringBuilder();
-            for (var index = position; index < position + length * 2; index += 2)
-                builder.Append(BitConverter.ToChar(_stream.ToArray(), index));
-            return builder.ToString();
-        }
-
-        #endregion
-
-        private void Write(byte value) =>
-            _stream.Add(value);
-
-        public void WriteDatatype(Datatype value) =>
-            Write((byte)value);
-
-        public void WriteDatatypes(IEnumerable<Datatype> value)
-        {
-            var datatypes = value.ToList();
-            Write((byte)datatypes.Count);
-            datatypes.ForEach(WriteDatatype);
-        }
-
-        public void WriteOperationCode(OperationCode value) =>
-            Write((byte)value);
-
-        public void WriteIndex(byte value) =>
-            Write(value);
-
-        public void WriteIdentifier(string value)
-        {
-            Write((byte)value.Length);
-            value.ToList().ForEach(@char => Write((byte)@char));
-        }
-
-        public void WriteDeclaration(FunctionDeclaration declaration)
-        {
-            WriteIdentifier(declaration.QualifiedName);
-            WriteDatatypes(declaration.Parameters);
-            WriteDatatype(declaration.Return);
-        }
-
-        #region Literal Writes
-
-        public void WriteBoolean(bool value) =>
-            BitConverter.GetBytes(value).ToList().ForEach(Write);
-
-        public void WriteInteger(long value) =>
-            BitConverter.GetBytes(value).ToList().ForEach(Write);
-
-        public void WriteFloat(double value) =>
-            BitConverter.GetBytes(value).ToList().ForEach(Write);
-
-        public void WriteString(string value)
-        {
-            Write((byte)value.Length);
-            value.ToList().ForEach(@char => BitConverter.GetBytes(@char).ToList().ForEach(Write));
-        }
-
-        #endregion
-
-        public IEnumerator<byte> GetEnumerator() =>
-            _stream.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() =>
-            _stream.GetEnumerator();
-    }
-
-    public readonly struct FunctionDeclaration : IEquatable<FunctionDeclaration>
-    {
-        public string QualifiedName { get; }
-
-        public Datatype Return { get; }
-
-        public IEnumerable<Datatype> Parameters { get; }
-
-        public FunctionDeclaration(string qualifiedName, Datatype @return, IEnumerable<Datatype> parameters)
-        {
-            QualifiedName = qualifiedName;
-            Return = @return;
-            Parameters = parameters;
-        }
-
-        public bool Equals(FunctionDeclaration declaration) =>
-            QualifiedName == declaration.QualifiedName && Return == declaration.Return
-                && Parameters.SequenceEqual(declaration.Parameters);
-
-        public override bool Equals(object? obj) =>
-            obj is FunctionDeclaration declaration && Equals(declaration);
-
-        public override int GetHashCode() =>
-            base.GetHashCode();
-    }
-
-    public sealed class FunctionImplementation
-    {
-        #region Non-Volatile Data
-
-        public FunctionDeclaration Declaration { get; }
-
-        public Buffer Buffer { get; } = new Buffer();
-
-        public IList<(string name, int position)> Labels { get; } = new List<(string name, int position)>();
-
-        #endregion
-
-        #region Volatile Data
-
-        public (Datatype datatype, object value) ReturnValue { get; set; }
-
-        public IList<(Datatype datatype, object value)> ParameterValues { get; private set; } = new List<(Datatype datatype, object value)>();
-
-        public IList<(Datatype datatype, object value)> Locals { get; private set; } = new List<(Datatype datatype, object value)>();
-
-        public Stack<(Datatype datatype, object value)> Stack { get; private set; } = new Stack<(Datatype datatype, object value)>();
-
-        #endregion
-
-        public FunctionImplementation(string qualifedName, Datatype @return, params Datatype[] parameterTypes) =>
-            Declaration = new FunctionDeclaration(qualifedName, @return, parameterTypes);
-
-        public void Reset()
-        {
-            ReturnValue = default;
-            ParameterValues = new List<(Datatype datatype, object value)>();
-            Locals = new List<(Datatype datatype, object value)>();
-            Stack = new Stack<(Datatype datatype, object value)>();
-        }
-
-        public int GetLabelPosition(string name) =>
-            Labels.Single(label => label.name == name).position;
     }
 }
